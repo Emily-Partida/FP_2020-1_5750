93168735cf0a3c91d592c43e08e6d70783ea1a26
Emily-Partida <54519893+Emily-Partida@users.noreply.github.com>
2019-10-17 21:58:31 -0500

    Agregue tarea 3

--- TAREAS/1/main.c
+++ TAREAS/1/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main (int argc, char *argu[]){
-	char *x;
-	x=argu[1];
-	printf("%s\n",x);
+int main (int argc, char*argv[]){
+double x, y;
+x= atof(argv[1]);
+y= atof(argv[2]);
+printf("La suma es %g\n", x+y);
+printf("La resta es %g\n", x-y);
+printf("La multiplicación es %g\n", x*y);
+printf("La division es %.2lf\n", x/y);
 return 0;
 }
-
